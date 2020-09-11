675f2c07cdb185229acbf546fd3b955bbf098369
EstebanP <epinczinger@gmail.com>
2020-09-11 12:47:52 -0300

    drawing book solved

---
+++ ruby/anagram.rb
@@ -0,0 +1,3 @@
+# Link to the problem below
+# https://www.hackerrank.com/challenges/anagram/problem?h_r=internal-search
+
---
+++ ruby/drawing-book.rb
@@ -0,0 +1,17 @@
+# Link to the problem below
+# https://www.hackerrank.com/challenges/drawing-book/problem
+
+def pageCount(n, p)
+
+    dif = (n - p).to_f
+    num_pages = n.to_f / 2
+
+    if dif >= num_pages
+        p/2
+    elsif dif < num_pages && n.even?
+         p == n ? 0 : (dif + 1).to_i / 2
+    else 
+        p == n - 1 || p == n ? 0 : dif.to_i / 2
+    end
+
+end
