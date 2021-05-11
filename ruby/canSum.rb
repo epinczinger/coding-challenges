dbb6e6f8c9cbe24ea3f5613a00738771c64a539c
EstebanP <epinczinger@gmail.com>
2021-05-11 14:30:08 -0300

    canSum with memo

---
+++ ruby/canSum.rb
@@ -0,0 +1,15 @@
+def canSum(targetSum, numbers, memo = {})
+    return memo[targetSum] if memo.has_key?(targetSum)
+    return true if targetSum === 0
+    return false if targetSum < 0
+ 
+    numbers.each do |n|
+        remainder = targetSum - n
+        if canSum(remainder, numbers, memo) ===true
+            memo[targetSum] = true
+            return true
+        end
+    end
+
+    false
+end
