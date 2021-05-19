67c7c7c11363fae5da5a1c69cb611d987d4c4406
EstebanP <epinczinger@gmail.com>
2021-05-19 18:53:28 -0300

    tabulation count Construct js

---
+++ javascript/tabulation/countConstruct.js
@@ -0,0 +1,17 @@
+const countConstruct = (target, wordBank) => {
+  const table = Array(target.length + 1).fill(0);
+
+  table[0] = 1;
+
+  for (let i = 0; i <= target.length; i++) {
+      for (let word of wordBank) {
+          if (target.slice(i, i + word.length) === word) {
+              table[i+word.length] += table[i];
+          }
+      }
+  }
+  return table[target.length];
+};
+
+
+console.log(countConstruct('purple', ['purp', 'le', 'pur', 'ple']));
